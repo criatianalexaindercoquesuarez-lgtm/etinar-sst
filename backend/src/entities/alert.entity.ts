6a9126d466b4cdda9b60052e01122377ddbf8407
criatianalexaindercoquesuarez-lgtm <criatianalexaindercoquesuarez@gmail.com>
2026-08-24 13:32:07 -0500

    Refactor Alert entity and update properties

--- backend/src/entities/alert.entity.ts
+++ backend/src/entities/alert.entity.ts
@@ -1,30 +1,35 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+} from 'typeorm';
 import { Document } from './document.entity';
-
-export enum AlertType {
-  DOCUMENTO_POR_VENCER = 'documento_por_vencer',
-  DOCUMENTO_VENCIDO = 'documento_vencido',
-  DOCUMENTO_RECHAZADO = 'documento_rechazado',
-  DOCUMENTO_OBSERVADO = 'documento_observado',
-  CONTRATISTA_SIN_DOCUMENTACION = 'contratista_sin_documentacion',
-}
+import { Project } from './project.entity';
 
 @Entity('alerts')
 export class Alert {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ type: 'varchar' })
-  type: AlertType;
-
-  @ManyToOne(() => Document, { nullable: true, onDelete: 'CASCADE' })
-  document: Document;
+  @Column()
+  title: string;
 
   @Column({ type: 'text' })
   message: string;
 
+  @Column({ default: 'warning' })
+  type: string;
+
   @Column({ default: false })
-  resolved: boolean;
+  isRead: boolean;
+
+  @ManyToOne(() => Document, { nullable: true, onDelete: 'CASCADE' })
+  document: Document;
+
+  @ManyToOne(() => Project, { nullable: true, onDelete: 'CASCADE' })
+  project: Project;
 
   @CreateDateColumn()
   createdAt: Date;
