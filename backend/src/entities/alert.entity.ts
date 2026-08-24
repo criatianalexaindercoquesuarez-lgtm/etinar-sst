b60adc95952fb18a9a2bc6b8b3a4ec35df52878a
criatianalexaindercoquesuarez-lgtm <criatianalexaindercoquesuarez@gmail.com>
2026-08-24 13:38:21 -0500

    Add AlertType enum and update Alert entity

--- backend/src/entities/alert.entity.ts
+++ backend/src/entities/alert.entity.ts
@@ -8,22 +8,33 @@ import {
 import { Document } from './document.entity';
 import { Project } from './project.entity';
 
+export enum AlertType {
+  DOCUMENTO_POR_VENCER = 'documento_por_vencer',
+  DOCUMENTO_VENCIDO = 'documento_vencido',
+  DOCUMENTO_RECHAZADO = 'documento_rechazado',
+  DOCUMENTO_OBSERVADO = 'documento_observado',
+  CONTRATISTA_SIN_DOCUMENTACION = 'contratista_sin_documentacion',
+}
+
 @Entity('alerts')
 export class Alert {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
+  @Column({ nullable: true })
   title: string;
 
   @Column({ type: 'text' })
   message: string;
 
-  @Column({ default: 'warning' })
-  type: string;
+  @Column({ type: 'varchar', default: 'warning' })
+  type: AlertType | string;
 
   @Column({ default: false })
   isRead: boolean;
+
+  @Column({ default: false })
+  resolved: boolean;
 
   @ManyToOne(() => Document, { nullable: true, onDelete: 'CASCADE' })
   document: Document;
