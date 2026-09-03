c5f575dd3ab95d136a02382aa8df09a29d6b3b25
criatianalexaindercoquesuarez-lgtm <criatianalexaindercoquesuarez@gmail.com>
2026-09-03 17:26:14 -0500

    Add DocumentType to TypeOrmModule imports

--- backend/src/projects/projects.module.ts
+++ backend/src/projects/projects.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from '../entities/project.entity';
 import { Folder } from '../entities/folder.entity';
+import { DocumentType } from '../entities/document-type.entity';
 import { ContractorProject } from '../entities/contractor-project.entity';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
@@ -9,7 +10,7 @@ import { CommonModule } from '../common/common.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Project, Folder, ContractorProject]),
+    TypeOrmModule.forFeature([Project, Folder, DocumentType, ContractorProject]),
     CommonModule,
   ],
   providers: [ProjectsService],
