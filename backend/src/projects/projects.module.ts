44015e9024f061db982a1b39b9b128d87c7e1b90
criatianalexaindercoquesuarez-lgtm <criatianalexaindercoquesuarez@gmail.com>
2026-09-09 22:19:44 -0500

    Add UserProjectsModule to ProjectsModule imports

--- backend/src/projects/projects.module.ts
+++ backend/src/projects/projects.module.ts
@@ -7,11 +7,13 @@ import { ContractorProject } from '../entities/contractor-project.entity';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
 import { CommonModule } from '../common/common.module';
+import { UserProjectsModule } from '../user-projects/user-projects.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Project, Folder, DocumentType, ContractorProject]),
     CommonModule,
+    UserProjectsModule,
   ],
   providers: [ProjectsService],
   controllers: [ProjectsController],
