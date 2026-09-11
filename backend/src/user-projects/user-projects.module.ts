8dd7ba3741b938ee16de132a24f578528c4e65b2
criatianalexaindercoquesuarez-lgtm <criatianalexaindercoquesuarez@gmail.com>
2026-09-11 09:02:34 -0500

    Add User and Project entities to UserProjectsModule

--- backend/src/user-projects/user-projects.module.ts
+++ backend/src/user-projects/user-projects.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserProject } from '../entities/user-project.entity';
+import { User } from '../entities/user.entity';
+import { Project } from '../entities/project.entity';
 import { UserProjectsService } from './user-projects.service';
 import { UserProjectsController } from './user-projects.controller';
+import { CommonModule } from '../common/common.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserProject])],
-  controllers: [UserProjectsController],
+  imports: [TypeOrmModule.forFeature([UserProject, User, Project]), CommonModule],
   providers: [UserProjectsService],
+  controllers: [UserProjectsController],
   exports: [UserProjectsService],
 })
 export class UserProjectsModule {}
