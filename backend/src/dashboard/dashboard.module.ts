49320139e771cdd5123200e94d881c09cb439897
criatianalexaindercoquesuarez-lgtm <criatianalexaindercoquesuarez@gmail.com>
2026-09-25 21:27:58 -0500

    Add ContractorProject entity to DashboardModule imports

--- backend/src/dashboard/dashboard.module.ts
+++ backend/src/dashboard/dashboard.module.ts
@@ -6,12 +6,23 @@ import { Document } from '../entities/document.entity';
 import { Alert } from '../entities/alert.entity';
 import { Worker } from '../entities/worker.entity';
 import { Sanction } from '../entities/sanction.entity';
+import { ContractorProject } from '../entities/contractor-project.entity';
 import { DashboardService } from './dashboard.service';
 import { DashboardController } from './dashboard.controller';
+import { UserProjectsModule } from '../user-projects/user-projects.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Project, Contractor, Document, Alert, Worker, Sanction]),
+    TypeOrmModule.forFeature([
+      Project,
+      Contractor,
+      Document,
+      Alert,
+      Worker,
+      Sanction,
+      ContractorProject,
+    ]),
+    UserProjectsModule,
   ],
   providers: [DashboardService],
   controllers: [DashboardController],
